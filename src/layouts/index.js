adb0018b53f58e3ab5f1375d77ded93821dd1a23
Victor Kariuki <karizvic@gmail.com>
2020-07-06 15:44:39 +0300

    initial commit

--- src/layouts/index.js
+++
@@ -1,9 +0,0 @@
-import AuthLayout from './AuthLayout';
-import DashboardLayout from './DashboardLayout';
-import PageLayout from './PageLayout';
-
-export {
-  AuthLayout,
-  DashboardLayout,
-  PageLayout,
-}
