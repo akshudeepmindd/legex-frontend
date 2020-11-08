9630a5bfe116c48d6a7f7184a8ca5c9444404d42
Naman Goyal <namangoyal4900@gmail.com>
2020-11-08 16:01:06 +0530

    emoved timeout on axios requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,19 @@
-import axios from "axios";
+import axios from "axios"
 
 const $http = () => {
-  const baseAxios = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    timeout: 5000,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
-  //get token from local storage
-  const token = localStorage.getItem("access-token");
-  if (token) {
-    baseAxios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  }
-  return baseAxios;
-};
+	const baseAxios = axios.create({
+		baseURL: process.env.REACT_APP_BASE_URL,
+		headers: {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+	})
+	//get token from local storage
+	const token = localStorage.getItem("access-token")
+	if (token) {
+		baseAxios.defaults.headers.common.Authorization = `Bearer ${token}`
+	}
+	return baseAxios
+}
 
-export default $http;
+export default $http
