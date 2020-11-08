1d9531fbf17bf7c74d58e5579cb47cfa291a73e5
Nishit Khaitan <nishitk@gmail.com>
2020-11-09 01:17:49 +0530

    Remove issues

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,6 @@
 import axios from "axios"
 
 const $http = () => {
-<<<<<<< HEAD
-  const baseAxios = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
-  //get token from local storage
-  const token = localStorage.getItem("access-token");
-  if (token) {
-    baseAxios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  }
-  return baseAxios;
-};
-=======
 	const baseAxios = axios.create({
 		baseURL: process.env.REACT_APP_BASE_URL,
 		headers: {
@@ -31,6 +15,5 @@ const $http = () => {
 	}
 	return baseAxios
 }
->>>>>>> 9630a5bfe116c48d6a7f7184a8ca5c9444404d42
 
 export default $http
