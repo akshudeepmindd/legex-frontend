d11a3e078b51321579bc9f743b8c461985b2c3a1
Nishit Khaitan <nishitk@gmail.com>
2020-11-25 15:51:51 +0530

    Fix Route Protection

--- src/WithAuth.js
+++ src/WithAuth.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Login} from './views'
+import { Redirect } from 'react-router-dom'
 
 export const WithAuth = (Component) => {
     return () => {
         // Check if Authenticated
-        const user = localStorage.getItem('token') // Handle return user context if authenticated or null if not
-
+        const user = localStorage.getItem("access-token") // Handle return user context if authenticated or null if not
+        
         // If Logged in, it will render the Component.
         if (user) {
             return <Component />;
         } else {
-            return <Login/>
+            return <Redirect to="/login" />
         }
     };
 };
